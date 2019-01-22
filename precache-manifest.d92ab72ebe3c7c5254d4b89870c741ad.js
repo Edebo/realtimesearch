0b41410301409d9a4ee77a327a5adf49f556077a
Edebo <akandeadebowale0@gmail.com>
2019-01-22 10:41:24 +0100

    Updates

---
+++ precache-manifest.d92ab72ebe3c7c5254d4b89870c741ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "810fe8e69ef86ba5656b",
+    "url": "/realtimesearch/static/css/main.feaa8fb2.chunk.css"
+  },
+  {
+    "revision": "810fe8e69ef86ba5656b",
+    "url": "/realtimesearch/static/js/main.810fe8e6.chunk.js"
+  },
+  {
+    "revision": "55392f26da91096a0216",
+    "url": "/realtimesearch/static/js/1.55392f26.chunk.js"
+  },
+  {
+    "revision": "f8b061fc004484f93d93",
+    "url": "/realtimesearch/static/js/runtime~main.f8b061fc.js"
+  },
+  {
+    "revision": "dcfe15e45d9971d440d79039f0f16e90",
+    "url": "/realtimesearch/index.html"
+  }
+];
